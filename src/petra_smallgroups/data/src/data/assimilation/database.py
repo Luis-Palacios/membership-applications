3623fbce5f894a5df44789635270be00504252b3
Luis Palacios <lrpalaciosdev@gmail.com>
2026-08-20 14:33:16 -0600

    remove print of connection string

--- src/petra_smallgroups/data/src/data/assimilation/database.py
+++ src/petra_smallgroups/data/src/data/assimilation/database.py
@@ -4,9 +4,6 @@ from .config import settings
 
 ASSIMILATION_DATABASE_URL = str(settings.assimilation_database_url)
 
-print(f"ASSIMILATION_DATABASE_URL: {ASSIMILATION_DATABASE_URL}")
-
-
 def init_engine() -> Engine:
     engine = create_engine(ASSIMILATION_DATABASE_URL, echo=settings.debug)
     return engine
