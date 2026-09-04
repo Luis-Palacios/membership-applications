656238f6a5338bf1f59b71c3d665363519d54044
Luis Palacios <lrpalaciosdev@gmail.com>
2026-09-04 17:11:53 -0600

    add membership application detail endpoint

--- src/membership_applications/api/routers/applications.py
+++ src/membership_applications/api/routers/applications.py
@@ -1,17 +1,23 @@
-from typing import Annotated
+from typing import TYPE_CHECKING
 
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, HTTPException
 
-from membership_applications.api.dependencies import get_assimilation_db
+from membership_applications.api.dependencies import SessionDep
 from membership_applications.api.schemas.applications import (
     ApplicationApproval,
     ApplicationRejection,
     ApplicationStatus,
     MembershipApplicationBase,
+    MembershipApplicationDetailSchema,
 )
 from membership_applications.data.assimilation.models.membership_applications.services import (
+    get_membership_application_detail_by_id,
     get_recent_membership_applications,
+)
+
+if TYPE_CHECKING:
+    from membership_applications.data.assimilation.models.membership_applications.results import (
+    MembershipApplicationSummary,
 )
 
 router = APIRouter(prefix="/applications", tags=["applications"])
@@ -19,7 +25,7 @@ router = APIRouter(prefix="/applications", tags=["applications"])
 
 @router.get("/recents", name="get_recent_applications", description="Get recent membership applications")
 def get_recent_applications(
-    db: Annotated[Session, Depends(get_assimilation_db)], status: ApplicationStatus | None = None
+    db: SessionDep, status: ApplicationStatus | None = None
 ) -> list[MembershipApplicationBase]:
     result = get_recent_membership_applications(db)
     # TODO: Implement status filter
@@ -57,5 +63,24 @@ async def reject_application(application: ApplicationRejection) -> dict[str, str
 
 
 @router.get("/{application_id}")
-async def get_application(application_id: int) -> dict[str, str]:
-    return {"message": f"Application ID: {application_id}"}
+def get_application(application_id: int, db: SessionDep) -> MembershipApplicationDetailSchema:
+    membership_application: MembershipApplicationSummary | None = get_membership_application_detail_by_id(
+        db, application_id=application_id
+    )
+
+    if membership_application is None:
+        raise HTTPException(status_code=404, detail=f"Application with ID {application_id} not found.")
+
+    return MembershipApplicationDetailSchema(
+        application_id=membership_application.id,
+        person_id=membership_application.person_id,
+        person_full_name=membership_application.first_name + " " + membership_application.last_name,
+        generated_date=membership_application.generated_date,
+        fulfilment_date=membership_application.fulfilment_date,
+        is_fulfilled=membership_application.fulfilment_date is not None,
+        first_name=membership_application.first_name,
+        last_name=membership_application.last_name,
+        life_before=membership_application.life_before,
+        conversion=membership_application.conversion,
+        life_after=membership_application.life_after,
+    )
